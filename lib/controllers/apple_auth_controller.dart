f9f67793f103e19e9b29bf49e82e460f7c1bcfa3
Mir Md Mosarof Hossan Showrav <showravofficial@gmail.com>
2025-01-11 11:30:35 +0600

    Apple auth add

--- lib/controllers/apple_auth_controller.dart
+++ lib/controllers/apple_auth_controller.dart
@@ -1,5 +1,52 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+// import '../models/user_model.dart';
+
+// class AppleAuthController {
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//   Future<UserModel?> signInWithApple() async {
+//     try {
+//       // Trigger the Apple Sign-In request
+//       final appleCredential = await SignInWithApple.getAppleIDCredential(
+//         scopes: [
+//           AppleIDAuthorizationScopes.email,
+//           AppleIDAuthorizationScopes.fullName,
+//         ],
+//       );
+
+//       // Create OAuth Credential for Firebase
+//       final OAuthCredential credential = OAuthProvider("apple.com").credential(
+//         idToken: appleCredential.identityToken,
+//         accessToken: appleCredential.authorizationCode,
+//       );
+
+//       // Sign in to Firebase with Apple credential
+//       final UserCredential userCredential = await _auth.signInWithCredential(credential);
+//       final User? user = userCredential.user;
+
+//       if (user != null) {
+//         return UserModel(
+//           displayName: user.displayName ?? appleCredential.givenName,
+//           email: user.email ?? appleCredential.email,
+//           photoURL: null, // Apple does not provide a profile picture
+//         );
+//       }
+//     } catch (e) {
+//       print("Error during Apple Sign-In: $e");
+//     }
+//     return null;
+//   }
+
+//   Future<void> signOut() async {
+//     await _auth.signOut();
+//   }
+// }
+
+
+
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../models/user_model.dart';
 
 class AppleAuthController {
@@ -13,6 +60,12 @@ class AppleAuthController {
           AppleIDAuthorizationScopes.email,
           AppleIDAuthorizationScopes.fullName,
         ],
+        webAuthenticationOptions: WebAuthenticationOptions(
+          clientId: 'com.example.project_auth.service', // Replace with your Service ID
+          redirectUri: Uri.parse(
+            'https://flutter-auth-test-221cb.firebaseapp.com/__/auth/handler', // Replace with your Redirect URI
+          ),
+        ),
       );
 
       // Create OAuth Credential for Firebase
@@ -36,9 +89,5 @@ class AppleAuthController {
       print("Error during Apple Sign-In: $e");
     }
     return null;
-  }
-
-  Future<void> signOut() async {
-    await _auth.signOut();
   }
 }
